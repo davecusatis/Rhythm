201338afb2daab5ea79d5ea43ae3fcf693d52a73
davecusatis <davecusatis1@gmail.com>
2015-10-31 16:06:26 -0400

    initial commit

---
+++ Rhythm/Engine/Section.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rhythm.Engine
+{
+    class Section
+    {
+        private String name;
+        private int[] notes;
+        private int BeatsPerMin;
+    }
+}
